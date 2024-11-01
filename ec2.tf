bc3960500c26655af5f0db97c77d680a67cd4bcc
Jose Manuel Marquez <goten60@gmail.com>
2024-11-01 09:10:28 -0300

    se documenta codigo y se remueve codigo que no se usa

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,4 @@
+#data para obtener la AMI amazon linux 2
 data "aws_ami" "amzLinux" {
     most_recent = true
     owners = ["amazon"]
@@ -19,20 +20,13 @@ data "aws_ami" "amzLinux" {
     }
 }
 
+#Security Group de EC2
 resource "aws_security_group" "ec2_sg" {
   vpc_id = module.vpc.vpc_id
   name = "ec2_sg"
 }
 
-/* resource "aws_vpc_security_group_ingress_rule" "allow_http" {
-  security_group_id = aws_security_group.ec2_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-}
- */
-
+#Regla de acceso para que el EC2 pueda acceder al RDS
 resource "aws_vpc_security_group_ingress_rule" "allow_rds" {
   security_group_id = aws_security_group.rds_sg.id
   referenced_security_group_id = aws_security_group.ec2_sg.id
@@ -41,59 +35,33 @@ resource "aws_vpc_security_group_ingress_rule" "allow_rds" {
   to_port           = 3306
 }
 
+#Regla de acceso para que el EC2 pueda acceder a internet
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.ec2_sg.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
+  ip_protocol       = "-1"
 }
 
-
-/* resource "aws_security_group" "pivote_sg" {
-  vpc_id = module.vpc.vpc_id
-  name = "pivote_sg"
-}
-
-
-resource "aws_vpc_security_group_ingress_rule" "pivote_ssh" {
-  security_group_id = aws_security_group.pivote_sg.id
-  cidr_ipv4         = var.ip_ssh
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
-
-resource "aws_vpc_security_group_egress_rule" "pivote_egress_ipv4" {
-  security_group_id = aws_security_group.pivote_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-} */
-
+#EC2 para el sistema wordpress
 resource "aws_instance" "web" {
-  #depends_on = [ aws_security_group.ec2_sg ]
-  #ami           = "ami-0c55b159cbfafe1f0"  # Reemplaza con la AMI de Linux que deseas
   ami = data.aws_ami.amzLinux.id
   instance_type = "t2.micro"
   metadata_options {
     http_tokens = "required"
   }
 
-  #subnet_id     = module.vpc.public_subnets[0]
-  subnet_id     = module.vpc.private_subnets[0]
+  subnet_id     = module.vpc.private_subnets[0] # Se pone en subnet privada ya que le trafico lo recibe el balanceador
   vpc_security_group_ids  = [aws_security_group.ec2_sg.id]
   tags = local.common_tags
   associate_public_ip_address = false
   root_block_device {
-    #device_name           = "/dev/sda1"
     volume_size           = 20
     volume_type           = "gp3"
     delete_on_termination = true
     encrypted = true
     tags = local.common_tags
   }
-  #iam_instance_profile = aws_iam_role.ssm_role.name  # Associating the role
   iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
-
-
 
   user_data = <<-EOF
               #!/bin/bash
@@ -115,8 +83,6 @@ resource "aws_instance" "web" {
               find /var/www -type d -exec sudo chmod 2775 {} \;
               find /var/www -type f -exec sudo chmod 0644 {} \;
               chown -R apache:apache /var/www
-
-
 
               # Configurar WordPress
               #cp wp-config-sample.php wp-config.php
@@ -143,7 +109,12 @@ resource "aws_instance" "web" {
                       "metrics_collection_interval": 60,
                       "resources": ["*"]
                     }
-                  }
+                  },
+                  "aggregation_dimensions": [
+                    [
+                        "InstanceId"
+                    ]
+                  ]
                 },
                 "logs": {
                   "logs_collected": {
@@ -174,32 +145,7 @@ resource "aws_instance" "web" {
               EOF
 }
 
-# Habilitar CloudWatch
-resource "aws_cloudwatch_log_group" "log_group" {
-  name = "AWS-logs-${var.ec2_log_group}"
-}
-
-/* resource "aws_instance" "pivote" {
-  #depends_on = [ aws_security_group.ec2_sg ]
-  #ami           = "ami-0c55b159cbfafe1f0"  # Reemplaza con la AMI de Linux que deseas
-  ami = data.aws_ami.amzLinux.id
-  instance_type = "t2.micro"
-  subnet_id     = module.vpc.public_subnets[0]
-  vpc_security_group_ids  = [aws_security_group.ec2_sg.id]
-  tags = local.common_tags
-  associate_public_ip_address = true
-  root_block_device {
-    #device_name           = "/dev/sda1"
-    volume_size           = 8
-    volume_type           = "gp3"
-    delete_on_termination = true
-    encrypted = true
-    tags = local.common_tags
-  }
-
-}
- */
-
+#Rol para la instancia
 resource "aws_iam_role" "ssm_role" {
   name = "SSM-Access-Role"
   assume_role_policy = <<-EOF
@@ -219,11 +165,13 @@ EOF
   tags = local.common_tags
 }
 
+#Insance profile EC2
 resource "aws_iam_instance_profile" "ssm_profile" {
   name = "ssm_profile"
   role = "${aws_iam_role.ssm_role.name}"
 }
 
+#Politicas necesarias para el rol
 resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
   role       = aws_iam_role.ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
@@ -238,6 +186,7 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
+#Alarma para el EC2 CPU
 resource "aws_cloudwatch_metric_alarm" "ec2_cpu_alarm" {
   alarm_name          = "EC2 High CPU Usage"
   comparison_operator = "GreaterThanThreshold"
@@ -255,6 +204,7 @@ resource "aws_cloudwatch_metric_alarm" "ec2_cpu_alarm" {
   alarm_actions = [aws_sns_topic.my_sns_topic.arn]
 }
 
+#Alarma para el EC2 Memoria
 resource "aws_cloudwatch_metric_alarm" "high_memory_usage" {
   alarm_name          = "EC2 HighMemoryUsage"
   comparison_operator = "GreaterThanThreshold"
@@ -272,6 +222,7 @@ resource "aws_cloudwatch_metric_alarm" "high_memory_usage" {
   alarm_actions = [aws_sns_topic.my_sns_topic.arn]
 }
 
+#Alarma para el EC2 Disco
 resource "aws_cloudwatch_metric_alarm" "high_disk_usage" {
   alarm_name          = "EC2 HighDiskUsage"
   comparison_operator = "GreaterThanThreshold"
@@ -284,24 +235,26 @@ resource "aws_cloudwatch_metric_alarm" "high_disk_usage" {
   alarm_description   = "Esta alarma se activa cuando el uso de disco supera el 90%."
   dimensions = {
     InstanceId = aws_instance.web.id
-    Partition  = "/"  # Cambia esto si necesitas monitorear una partición específica
+    Partition  = "/"
   }
 
   alarm_actions = [aws_sns_topic.my_sns_topic.arn]
 }
 
+#SNS topic para el envio de alertas
 resource "aws_sns_topic" "my_sns_topic" {
   name = "MyCloudWatchAlarms"
 }
 
+#SNS topic subscription
 resource "aws_sns_topic_subscription" "email_subscription" {
   topic_arn = aws_sns_topic.my_sns_topic.arn
   protocol  = "email"
   endpoint  = var.sns_email
 }
 
-output "instance_public_ip" {
-  value = aws_instance.web.public_ip
+output "instance_private_ip" {
+  value = aws_instance.web.private_ip
 }
 
 output "instance_id" {
